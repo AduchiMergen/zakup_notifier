d9ab26652d8e35d88c1447b3235c31fa3964504e
kdelinx <kdelinx@gmail.com>
2018-12-01 18:09:14 +0700

    add example settings_local

---
+++ src/project/settings/example_settings_local.py
@@ -0,0 +1,14 @@
+SECRET_KEY = 'z@g9tm#4^v$(p5+&%uvd#$@p=fpuwms=^81-(9-=fcgu#96+zc'
+
+DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'DB_NAME',
+        'HOST': '127.0.0.1',
+        'PORT': 5432,
+        'USER': 'django',
+        'PASSWORD': 'django'
+    }
+}
